76c3ecd902b50d8632bb9ca431ba0ed520b7c0d3
Isaac Gateño <isaac@gateno.net>
2011-11-07 17:25:35 -0800

    linear time random graph generator

--- randomgraph.cpp
+++ randomgraph.cpp
@@ -1,28 +1,38 @@
 #include <fstream>
 #include <iostream>
 #include <vector>
+#include <new>
 
 using namespace std;
 
-int main(int argc, char* argv[])
+bool** allocateGraph(int n)
 {
-  if (argc != 4) {
-    cout << "Wrong number of arguments" << endl;
-    return 1;
+  bool** graph = new (nothrow) bool*[n];
+  if (graph == NULL) {
+    cout << "Memory allocation error" << endl;
+    exit(1);
   }
-
-  int n = atoi(argv[1]);
-  int m = atoi(argv[2]);
-  char* outputFile = argv[3];
-
-  vector< vector<bool> > graph;
   for (int i = 0; i < n; i++) {
-    vector<bool> v;
-    for (int j = 0; j < n; j++) {
-      v.push_back(false);
+    graph[i] = new (nothrow) bool[n];
+    if (graph[i] == NULL) {
+      cout << "Memory allocation error" << endl;
+      exit(1);
     }
-    graph.push_back(v);
   }
+  return graph;
+}
+
+void deallocateGraph(bool** graph, int n)
+{
+  for (int i = 0; i < n; i++) {
+    delete(graph[i]);
+  }
+  delete(graph);
+}
+
+void outputRandomGraph(int n, int m, char* outputFile)
+{
+  bool** graph = allocateGraph(n);
 
   std::ofstream os;
   os.open(outputFile);
@@ -42,5 +52,21 @@ int main(int argc, char* argv[])
   }
 
   os.close();
+
+  deallocateGraph(graph, n);
+}
+
+int main(int argc, char* argv[])
+{
+  if (argc != 4) {
+    cout << "Wrong number of arguments" << endl;
+    return 1;
+  }
+
+  int n = atoi(argv[1]);
+  int m = atoi(argv[2]);
+  char* outputFile = argv[3];
+  outputRandomGraph(n, m, outputFile);
+
   return 0;
 }
